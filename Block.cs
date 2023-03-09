56b63aa82b1a812cfaefb839d5c7952315e7b98c
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-03-08 20:34:37 -0800

    Implemented BitmapSource

--- Block.cs
+++ Block.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace Prog_122_W23_Lecture_15_TopicReview
@@ -13,20 +14,59 @@ namespace Prog_122_W23_Lecture_15_TopicReview
 
         string header;
         string body;
-          BitmapImage _photo;
+        BitmapSource _photo;
         Filter filter;
 
-        public string Header { get => header; set => header = value; }
-        public string Body { get => body; set => body = value; }
-        public BitmapImage Photo { get => _photo; set => _photo = value; }
-        internal Filter Filter1 { get => filter; set => filter = value; }
-
-        public Block(string header, string body, BitmapImage photo, Filter filter)
+        public Block(string header, string body, string filePath, Filter filter)
         {
             this.header = header;
             this.body = body;
-            _photo = photo;
             this.filter = filter;
+
+            if (filter == Filter.grayskill)
+            {
+                _photo = GenerateGreyScale(GenerateImage(filePath));
+
+            }
+            else
+            {
+                _photo = GenerateImage(filePath);
+            }
+    
+        }
+
+        public string Header { get => header; set => header = value; }
+        public string Body { get => body; set => body = value; }
+        public BitmapSource Photo { get => _photo; set => _photo = value; }
+        internal Filter Filter1 { get => filter; set => filter = value; }
+
+        private BitmapImage GenerateImage(string filePath)
+        {
+            // New URI
+            // New BitmapImage
+            //Uri uri = new Uri(filePath);
+            //BitmapImage bitmap = new BitmapImage(uri);
+            //return bitmap;
+            return new BitmapImage(new Uri(filePath));
+        }
+
+       
+
+        private FormatConvertedBitmap GenerateGreyScale(BitmapImage bitmap)
+        {
+            // Createa a new FormatCOnvertedBitmap
+            FormatConvertedBitmap format = new FormatConvertedBitmap();
+
+            // Begin Init
+            format.BeginInit();
+            // Attach Source
+            format.Source = bitmap;
+            // Change to greyscale
+            format.DestinationFormat = PixelFormats.Gray32Float;
+
+            // End Init
+            format.EndInit();
+            return format;
         }
 
         // Header - Body
