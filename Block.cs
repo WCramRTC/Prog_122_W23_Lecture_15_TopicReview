5c7669c3f8c7090a6edc073109bb9250cd54e809
breshna <breshna2018@gmail.come>
2023-03-08 20:16:16 -0800

    create class and made method overide

---
+++ Block.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace Prog_122_W23_Lecture_15_TopicReview
+{
+    internal class Block
+    {
+       public enum Filter { regualer,grayskill};
+
+        string header;
+        string body;
+          BitmapImage _photo;
+        Filter filter;
+
+        public string Header { get => header; set => header = value; }
+        public string Body { get => body; set => body = value; }
+        public BitmapImage Photo { get => _photo; set => _photo = value; }
+        internal Filter Filter1 { get => filter; set => filter = value; }
+
+        public Block(string header, string body, BitmapImage photo, Filter filter)
+        {
+            this.header = header;
+            this.body = body;
+            _photo = photo;
+            this.filter = filter;
+        }
+
+        // Header - Body
+        public override string ToString()
+        {
+            return $"{header}-{body}";
+        }
+    }
+}
